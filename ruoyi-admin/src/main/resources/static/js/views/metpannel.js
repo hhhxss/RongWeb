d6d9a781fc23386dbacde9406de4e71a9aba1421
xcl69 <1664629830@qq.com>
2019-12-25 17:03:38 +0800

    金桥镇雨量变化及对应接口api

--- ruoyi-admin/src/main/resources/static/js/views/metpannel.js
+++ ruoyi-admin/src/main/resources/static/js/views/metpannel.js
@@ -2,6 +2,8 @@ function met_onload(){
 
     //首页图表 村镇雨量分布可视化
     init_io_vraindata();
+    //首页图表 金桥镇雨量变化可视化
+    init_io_jqrain();
 
 }
 
@@ -15,7 +17,7 @@ function init_io_vraindata() {
             var x_data = new Array();
             //var y_data = new Array();
             for( x in vrain_data){
-                if(vrain_data[x].parea == ''){
+                if(vrain_data[x].rname == ''){
                     x_data.push('未知');
                 }else {
                     x_data.push(vrain_data[x].rname);
@@ -64,6 +66,83 @@ function init_io_vraindata() {
                 ]
             };
             io_vraindata.setOption(io_vraindata_option);
+
+        }
+    })
+}
+
+function init_io_jqrain() {
+    $.ajax({
+        type: "GET",
+        url: "/api/jqrain/time",
+        dataType: "json",
+        success: function (data) {
+            var jqrain_data = data.data;
+            var x_data = new Array();
+            var y_data = new Array();
+            for( x in jqrain_data){
+                if(jqrain_data[x].time == ''){
+                    x_data.push('未知');
+                }else {
+                    x_data.push(jqrain_data[x].time);
+                }
+            }
+            for (y in jqrain_data){
+                y_data.push(jqrain_data[y].data);
+            }
+
+            var io_jqrain = echarts.init(document.getElementById('io_jqrain'));
+            io_jqrain_option = {
+                tooltip: {
+                    trigger: 'axis',
+                    axisPointer: {
+                        type: 'cross',
+                        crossStyle: {
+                            color: '#999'
+                        }
+                    }
+                },
+                toolbox: {
+                    feature: {
+                        dataView: {show: true, readOnly: false},
+                        magicType: {show: true, type: ['line', 'bar']},
+                        restore: {show: true},
+                        saveAsImage: {show: true}
+                    }
+                },
+                // legend: {
+                //     data:['蒸发量','降水量','平均温度']
+                // },
+                xAxis: [
+                    {
+                        type: 'category',
+                        data: x_data,
+                        axisPointer: {
+                            type: 'shadow'
+                        }
+                    }
+                ],
+                yAxis: [
+                    {
+                        type: 'value',
+                        name: '水量',
+                        min: 0,
+                        max: 12,
+                        interval: 2,
+                        axisLabel: {
+                            formatter: '{value} ml'
+                        }
+                    }
+                ],
+                series: [
+                    {
+                        name:'降水量',
+                        type:'bar',
+                        data:y_data
+                    }
+                ]
+            };
+            io_jqrain.setOption(io_jqrain_option);
 
         }
     })
