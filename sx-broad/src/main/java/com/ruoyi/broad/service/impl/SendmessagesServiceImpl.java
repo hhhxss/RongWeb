c4e197b2eec3675f6ad7bcf06bc54224845f62b3
hhhxss <2776348774@qq.com>
2019-12-26 21:22:24 +0800

    广播发送信息类型可视化以及对应接口api

--- sx-broad/src/main/java/com/ruoyi/broad/service/impl/SendmessagesServiceImpl.java
+++ sx-broad/src/main/java/com/ruoyi/broad/service/impl/SendmessagesServiceImpl.java
@@ -86,5 +86,10 @@ public class SendmessagesServiceImpl implements ISendmessagesService
 	{
 		return sendmessagesMapper.deleteSendmessagesByIds(Convert.toStrArray(ids));
 	}
-	
+
+	@Override
+	public List<Sendmessages> selectSendByremark() {
+		return sendmessagesMapper.selectSendByremark();
+	}
+
 }
