27840105cebe352fd7909e8b546f1992d12cf2a8
jiaxinru <3211911476@qq.com>
2019-12-27 15:43:04 +0800

    榔梨街道河流水位可视化

---
+++ sx-iot/src/main/java/com/ruoyi/iot/mapper/LangLiJieDaoMapper.java
@@ -0,0 +1,30 @@
+package com.ruoyi.iot.mapper;
+
+import com.ruoyi.iot.domain.LangLiJieDao;
+import java.util.List;
+
+/**
+ * 榔梨街道河流水位监测表
+ *
+ * @author 贾欣如
+ * @date 2019-12-27
+ */
+
+public interface LangLiJieDaoMapper {
+    /**
+     * 查询河流信息
+     *
+     * @param id 村镇ID
+     * @return 河流信息
+     */
+    public LangLiJieDao selsectLangLiJieDaoById(Integer id);
+
+    /**
+     * 查询温度信息
+     *
+     * @param :查询榔梨街道河流信息
+     * @return 查询河流信息
+     */
+    public List<LangLiJieDao> selectLangLiJieDaoList(LangLiJieDao langlijiedao);
+    public List<LangLiJieDao> selectLangLiJieDaoBytime();
+}
