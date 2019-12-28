41a24defc166d42aacaa2c28f42798bf63c859e0
chengxuyuanczczc <1547048069@qq.com>
2019-12-28 22:38:19 +0800

    山洪日志信息表格可视化及log对应controller

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/rivervis/RiverPannelController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/rivervis/RiverPannelController.java
@@ -1,9 +1,17 @@
 package com.ruoyi.web.controller.rivervis;
 
+import com.ruoyi.common.page.TableDataInfo;
 import com.ruoyi.framework.web.base.BaseController;
+import com.ruoyi.rivervis.domain.logss;
+import com.ruoyi.rivervis.service.IlogssService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 /**
  * @author 张超 teavamc
@@ -16,8 +24,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class RiverPannelController extends BaseController {
     private String prefix = "rivervis/pannel";
 
+    @Autowired
+    private IlogssService logssService;
+
     @GetMapping()
     public String pannel() {
         return prefix + "/pannel";
+    }
+
+    @PostMapping("/list")
+    @ResponseBody
+    public TableDataInfo list() {
+        startPage();
+        List<logss> list = logssService.select();
+        return getDataTable(list);
     }
 }
