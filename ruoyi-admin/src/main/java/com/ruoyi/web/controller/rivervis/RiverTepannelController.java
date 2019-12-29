6528957fb45c042b8132f942b389373ea8aa5acd
hhhxss <2776348774@qq.com>
2019-12-29 18:05:43 +0800

    终端数据图表数据可视化以及对应接口

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/rivervis/RiverTepannelController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/rivervis/RiverTepannelController.java
@@ -1,9 +1,18 @@
 package com.ruoyi.web.controller.rivervis;
 
+import com.ruoyi.broad.domain.BroadCount;
+import com.ruoyi.common.page.TableDataInfo;
 import com.ruoyi.framework.web.base.BaseController;
+import com.ruoyi.rivervis.domain.terminalsdata;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import com.ruoyi.rivervis.service.IterminalsdataService;
+
+import java.util.List;
 
 /**
  * @author xss
@@ -15,9 +24,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/rivervis/tepannel")
 public class RiverTepannelController extends BaseController{
     private String prefix = "rivervis/tepannel";
-
+    @Autowired
+    private IterminalsdataService iterminalsdataService;
     @GetMapping()
     public String tepannel() {
         return prefix + "/tepannel";
+    }
+
+    @PostMapping("/list")
+    @ResponseBody
+    public TableDataInfo list() {
+        startPage();
+        List<terminalsdata> list = iterminalsdataService.select();
+        return getDataTable(list);
     }
 }
