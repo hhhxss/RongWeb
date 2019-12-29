a44f5b2360c02058681ceec41511562a22d74401
jiaxinru <3211911476@qq.com>
2019-12-29 20:38:19 +0800

    村务功能类型

---
+++ sx-village/src/main/java/com/ruoyi/village/mapper/WcmenuMapper.java
@@ -0,0 +1,29 @@
+package com.ruoyi.village.mapper;
+
+import com.ruoyi.village.domain.Wcmenu;
+import java.util.List;
+/**
+ * 村务功能类型
+ *
+ * @author 贾欣如
+ * @date 2019-12-29
+ */
+
+public interface WcmenuMapper {
+    /**
+     * 村务功能类型
+     *
+     * @param wmid 类型WID
+     * @return 村务功能类型
+     */
+    public Wcmenu selsectWcmenuByWmid(Integer wmid);
+
+    /**
+     * 村务功能类型
+     *
+     * @param :村务功能类型
+     * @return 村务功能类型
+     */
+    public List<Wcmenu> selectWcmenuList(Wcmenu wcmenu);
+    public List<Wcmenu> selectWcmenuBymname();
+}
